b50a569f927bc9abaf3678b28482b29bec9a0937
highfurby <danae.flores.sa@gmail.com>
2021-07-19 18:53:07 +0200

    cmmit

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,21 @@
 var myCarousel = document.querySelector('#carouselExampleControls')
 var carousel = new bootstrap.Carousel(myCarousel, {
-  pause: false
+  pause: true
 })
+
+$(function () {
+
+  $("a").click(function (event) {
+
+      if (this.hash !== "") {
+          event.preventDefault();
+
+          var gato = this.hash;
+
+          $("html, body").animate({
+              scrollTop: $(gato).offset().top
+          }, 800, function () {
+          });
+      }
+  });
+});
